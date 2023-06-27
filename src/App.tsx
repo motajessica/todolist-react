9c86eb18d1f69d54526e2e8b702ec35503dc6a27
motajessica <motajessicaa@gmail.com>
2023-06-27 15:45:56 +1200

    Add tasklist and taskitem

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 
 //custom components
 import CustomForm from './components/CustomForm'
+import TaskList from './components/TaskList'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tasks, setTasks] = useState([])
 
-  const addTask = (task) => {
-    console.log(task)
+  const addTask = (task: Task) => {
+    setTasks(prevState => [ ... prevState, task ]);
   }
   return (
       <div className='container'>
@@ -17,6 +18,7 @@ function App() {
           <h1>My Task List</h1>
         </header>
         <CustomForm addTask={addTask}/>
+        {tasks && <TaskList tasks={tasks} />}
       </div>
   )
 }
---
+++ src/components/TaskItem.tsx
@@ -0,0 +1,9 @@
+const TaskItem = ({task}) => {
+  return (
+    <div>
+      {task.name}
+    </div>
+  )
+}
+
+export default TaskItem
